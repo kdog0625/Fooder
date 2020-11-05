aba7680873c0b4298b00e09c4ac658a7e63cd971
kdog0625 <kdoooog34@gmail.com>
2020-11-05 23:59:40 +0900

    tweetモデル作成

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -4,24 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Tweet;
+
 class TweetController extends Controller
 {
     public function index()
     {
-    //ダミーデータ追加
-    $tweets = [
-        (object) [
-            'id' => 1,
-            'title' => 'タイトル1',
-            'content' => '本文1',
-            'created_at' => now(),
-            'user' => (object) [
-                'id' => 1,
-                'usernoid' => '@manapon1',
-                'name' => 'ユーザー名1',
-            ],
-        ],
-    ];
+    // 
+    $tweets = Tweet::all()->sortByDesc('created_at');
+
     return view('tweets.index', ['tweets' => $tweets]);
     }
 }
