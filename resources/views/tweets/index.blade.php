2d37d4781366a7a786257880222c4ace41cc3a18
kdog0625 <kdoooog34@gmail.com>
2020-11-06 00:07:46 +0900

    tweet.indexのビュー変更

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -3,35 +3,15 @@
 @section('title', 'Food一覧')
 
 @section('content')
-<div class="container">
-@foreach($tweets as $tweet) 
-      <div class="card mt-3">
-        <div class="card-body d-flex flex-row">
-          <i class="fas fa-user-circle fa-3x mr-1"></i>
-          <div>
-            <div class="font-weight-bold">
-              {{ $tweet->user->name }} 
-            </div> 
-            <div class="font-weight-lighter">
-              {{ $tweet->created_at->format('Y/m/d H:i') }}
-            </div>
-          </div>
-        </div>
-        <div class="card-body pt-0 pb-2">
-          <h3 class="h4 card-title">
-            {{ $tweet->title }} 
-          </h3>
-          <div class="card-text">
-            {!! nl2br(e( $tweet->content )) !!} 
-          </div>
-        </div>
-      </div>
-    @endforeach
+@include('nav')
+<!-- <div class="jumbotron"  style="background:url(images/bacl.jpg); background-size:cover;">
+  </div> -->
+  <div class="imgwrapper">
+   <img src="images/image01.jpg" class="img-responsive" style="width: 100%; background-size:cover">
 </div>
-
-
-
+<div class="container">
+<div class="row">
+      
+    </div>
+</div>
 @endsection
-
-
-<div class="container" style="margin-top:50px;">
