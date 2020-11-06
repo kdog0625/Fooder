321edc0fddd083e59bfba81118a867c0ba28ed63
kdog0625 <kdoooog34@gmail.com>
2020-11-07 00:11:27 +0900

    fillable設定

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -26,8 +26,7 @@ class TweetController extends Controller
     //storeアクションメソッドは、第一引数が$request。引数$requestはTweetRequestクラスのインスタンスである、ということを宣言
     public function store(TweetRequest $request, Tweet $tweet)
     {
-        $tweet->title = $request->title;
-        $tweet->content = $request->content;
+        $tweet->fill($request->all()); 
         $tweet->user_id = $request->user()->id;
         $tweet->save();
         return redirect('/');
